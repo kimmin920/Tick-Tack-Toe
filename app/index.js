0d2e04b9e07d36c4db7621ead711113353a65eea
kimmin920 <vvee1253@gmail.com>
2020-10-15 20:11:02 +0900

    calculate winner

--- app/index.js
+++ app/index.js
@@ -46,8 +46,7 @@ function calculateWinner(squares) {
   ];
 
   for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-
+    const [a, b, c] = lines[i];  // 알아보기! 비구조화 할당?
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
@@ -62,37 +61,45 @@ function calculateWinner(squares) {
 
  */
 
+//  ox 한번 등록되면 덮혀지지않게 하기
+
 const boxsNodeList = document.querySelectorAll(".board__box");
 const boxsArray = Array.from(boxsNodeList);
 
 let arrayOfO = [];
 let arrayOfX = [];
-
-function turnChanger(){
-}
+let squares = [
+  null, null, null,
+  null, null, null,
+  null, null, null,
+];
 
 function paintBox(targetBox){
   const oMark = "⭕";
   const xMark = "❌";
-
   if(arrayOfO.length === arrayOfX.length){
     targetBox.textContent = oMark;
     arrayOfO.push(targetBox.id);
     console.log(arrayOfO);
+    squares[targetBox.id] = "O";
   }else{
     targetBox.textContent = xMark;
     arrayOfX.push(targetBox.id);
     console.log(arrayOfX);
+    squares[targetBox.id] = "X";
   }
-
+  console.log(squares);
+  const result = calculateWinner(squares);
+  result && console.log(`winner is ${result}`);
 }
 
 function handleClick(e){
   const targetId = e.target.id;
-  const targetBox = document.getElementById(`${targetId}`); // document.querySelector(`#${targetId}`) 는 오류가 남, 알아보기.
+  const targetBox = document.getElementById(`${targetId}`); // document.querySelector(`#${targetId}`) 는 안됨. 알아보기.
   paintBox(targetBox);
 }
 
 
 boxsArray.map(e=> e.addEventListener("click", handleClick));
+
 
