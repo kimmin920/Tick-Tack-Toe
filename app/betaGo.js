409a2c4aaa895bcc1a1228e0c80819b9583b3906
kimmin920 <vvee1253@gmail.com>
2020-10-18 01:23:16 +0900

    beta-Go error fix

--- app/betaGo.js
+++ app/betaGo.js
@@ -1,4 +1,6 @@
 const betagoBtn = document.querySelector(".versusBtn");
+const player2Board = document.querySelector(".player2");
+const player2Text = document.querySelector(".player2Text");
 
 function getBoxForBetago(){
   let betagoObj = {
@@ -19,10 +21,18 @@ function handleBetagoClick(e){
     paintContent(betagoBtn, "배타고 ON");
     betagoBtn.style.backgroundColor = "rgb(248, 255, 143)";
     betagoBtn.style.color = "rgb(235, 147, 135)";
+
+    // paintContent(player2Board, "배타고🚢(초급)"); textContent를 수정하니, 해당 div의 하위 div들이 사라지더라구요..
+    // textContent는 text만 바꾼다고 이해했는데, 잘못 이해했나봐요. text만 바꾸고 내부 div는 안건들이는 방법이 있나요?
+
+    paintContent(player2Text, "배타고🚢(초보)")
+    player2Board.style["font-size"] = "35px";
   }else{
     paintContent(betagoBtn, "배타고 OFF")
     betagoBtn.style.backgroundColor = "grey";
     betagoBtn.style.color = "white";
+    paintContent(player2Text, "백색이");
+    player2Board.style["font-size"] = "3rem";
   }
 }
 
